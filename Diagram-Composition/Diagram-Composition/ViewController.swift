11f47aea300d37a3ddb8fea4d203c23fcbad8be8
Subhankar <subhankaracharya@gmail.com>
2023-04-03 13:03:20 +0100

    added type composition

--- Diagram-Composition/Diagram-Composition/ViewController.swift
+++ Diagram-Composition/Diagram-Composition/ViewController.swift
@@ -10,33 +10,20 @@ import UIKit
 protocol FeedLoader {
     func loadFeed(completion: @escaping ([String]) -> Void)
 }
-
-struct Rechability {
-    static let networkAvailable = false
-}
-
 class FeedViewController: UIViewController {
-    // both concrete dependencies
-    var remote: RemoteFeedLoader!
-    var local: LocalFeedLoader!
+    var loader: FeedLoader!
     
-    convenience init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+    convenience init(loader: FeedLoader) {
         self.init()
-        self.remote = remote
-        self.local = local
+        self.loader = loader
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if Rechability.networkAvailable {
-            remote.loadFeed { loadedItems in
-                // do something
-            }
-        } else {
-            local.loadFeed { loadedItems in
-                // do something
-            }
+        loader.loadFeed { loadedItems in
+            // update UI
+            print(loadedItems)
         }
     }
 }
@@ -52,5 +39,34 @@ class LocalFeedLoader: FeedLoader {
     func loadFeed(completion: @escaping ([String]) -> Void) {
         // do something
         completion(["result3, result4"])
+    }
+}
+
+struct Rechability {
+    static let networkAvailable = false
+}
+
+/// This class composes the 2 concrete types 
+class RemoteWithLocalFallbackFeedLoader: FeedLoader {
+    // both concrete dependencies
+    var remote: RemoteFeedLoader!
+    var local: LocalFeedLoader!
+    
+    convenience init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+        self.init()
+        self.remote = remote
+        self.local = local
+    }
+    
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        if Rechability.networkAvailable {
+            remote.loadFeed { loadedItems in
+                // do something
+            }
+        } else {
+            local.loadFeed { loadedItems in
+                // do something
+            }
+        }
     }
 }
